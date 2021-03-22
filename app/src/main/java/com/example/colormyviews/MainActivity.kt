7c84f7aff54a35c0bba6d71a7d6b030493136749
Claylton dos Santos Pinto <clayltonsp@hotmail.com>
2021-03-21 21:01:03 -0300

    Concluindo a aplicação
    Adicionando novos botões para pintar alguns quadrados
    
    Layout totalmente redimensionável

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -20,7 +20,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners(){
         val clickableViews: List<View> =
             listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
-                binding.boxFourText, binding.boxFiveText, binding.constrainLayout)
+                binding.boxFourText, binding.boxFiveText, binding.constrainLayout,
+                binding.redButton, binding.yellowButton, binding.greenButton)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -38,6 +39,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
